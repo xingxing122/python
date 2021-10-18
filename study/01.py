e2a2b4200377db5bac349d4830261cfd3c2b5ba6
Xingxing <xingxingwang122@gmail.com>
2021-10-18 17:57:08 +0800

    python

---
+++ study/01.py
@@ -0,0 +1,29 @@
+
+"""
+name="xingxing"
+print(name)
+
+a =1
+b = 2
+print(a+b)
+
+
+a = b = 1
+print(a+b)
+
+calc  = 100/88
+print("计算结果: %.2f" %calc)
+
+name="xing"
+age=30
+print("姓名: %s,\"年龄: %d" %(name,age))
+"""
+# 3.获取用户输入
+
+age = input("你叫什么名字:")
+print(age)
+
+
+
+
+
